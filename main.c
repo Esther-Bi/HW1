dc502d35ed2a7485ec691fe41a1cd09a3acd3acd
Esther Binnes <estherbinnes@gmail.com>
2021-11-03 18:10:40 +0200

    update main.c

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ int main(){
     for (int i=a ; i<=b ; i++){
         if (isPalindrome(i)==1) printf(" %d" , i);
     }
-    printf("The Prime numbers are:")
+    printf("The Prime numbers are:");
     for (int i=a ; i<=b ; i++){
         if (isPrime(i)==1) printf(" %d" , i);
     }
